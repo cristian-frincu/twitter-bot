403271ff7d593f6db9134e882427dc6496f689ca
Cristian Frincu <ccfrincu@gmail.com>
2015-02-17 21:19:35 -0500

    added sigmoid calculations

--- Node.py
+++ Node.py
@@ -13,3 +13,17 @@ def makeMatrix(I,J,fill=0.0):
 		m.append([fill]*J)
 	return m
 
+#the standard way to calculate the sigmoid is the 1/(1+x^-x), but tanh gives a close aprozimation
+# and it will chose whichever gives better results
+def sigmoidTanh(x):
+	return math.tanh(x)
+
+def sigmoidStandard(x):
+	return 1/(1+exp(-x))
+
+#derivatives of the sigmoids respectively
+def dsigmoidTanh(y):
+	return 1.0 - y**2
+
+def dsigmoidStandard(y):
+	return sigmoidStandard(y)*(1-sigmoidStandard(y))
